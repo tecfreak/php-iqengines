8467c9bbcebb992e4362d017f4c78f93bcad5312
huy <huy@huyng.com>
2012-09-04 16:06:18 -0700

    cleaned up php sample code

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a bare bones example of using the query and update apis to send an image and retrieve results for that image. The scripts in this package will send in the included "sample.jpg" image.
+
+### Usage
+
+1. First change the API_KEY and API_SECRET variables in `query.php` and `update.php` to those provided in your developer dashboard [settings page][1].
+2. run `php query.php`
+3. run `php update.php`. If the call timeouts before you recieve a result you can re-run this script until you get a response.
+
+[1]: https://www.iqengines.com/dashboard/settings
--- update.php
+++ update.php
@@ -1,61 +1,51 @@
 <?php 	
-	//Url to the IQEngines Update api
+
+	date_default_timezone_set('UTC');
+
+	// Url to the IQEngines Update api
 	$url = "http://api.iqengines.com/v1.2/update/";
 	
-	//Get the current time in UTC Time Zone (Else we get client-server time mismatch)
-	date_default_timezone_set('UTC');
+	// API variables
+	$api_key = '<API_KEY>';
+    $api_secret = '<API_SECRET>';
     $timestamp = date('YmdHis');
-	
-	//API variables
-	$api_key = '<YOUR API_KEY>';
 	$json = '1';
-	$api_secret = '<YOUR API_SECRET>';
 	
-	//Compose the api raw signature string
-	$temp_api_sig = 'api_key'.$api_key.'json'.$json.'time_stamp'.$timestamp;
+	// Compose the api raw signature string
+	$raw_string = 'api_key'.$api_key.'json'.$json.'time_stamp'.$timestamp;
 	
-	//Encode the api signature
-	$api_sig = hash_hmac("sha1",$temp_api_sig,$api_secret,false);
+	// Encode the api signature
+	$api_sig = hash_hmac("sha1", $raw_string, $api_secret,false);
 	
-	//Just echo-ing all of my variables
-	/*trace('tempAPI: '.$temp_api_sig);
-	trace('img: '.$img);
-	trace('api_key: '.$api_key);
-	trace('api_secret: '.$api_secret);
-	trace('api_sig: '.$api_sig);
-	trace('timestamp: '.$timestamp);
-	trace('json: '.$json);*/
-	
-	//Preparing the data we will be sending
+	// Preparing the data we will be sending
 	$fields = array(
-		'api_key' => urlencode($api_key),
+		'api_key' => $api_key,
 		'api_sig' => $api_sig,
-		'time_stamp' => urlencode($timestamp),
-		'json' => urlencode($json)
+		'time_stamp' => $timestamp,
+		'json' => $json
 	);
 	
-	
-	//Url-ify the data for the POST 
+	// Url-ify the data for the POST 
 	$fields_string = "";
 	foreach($fields as $key=>$value) { 
 		$fields_string .= $key.'='.$value.'&'; 
 	}
 	rtrim($fields_string,'&');
 	
-	
-	//Here I make the cURL request to the API
+	// Here I make the cURL request to the API
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER  ,true);  // RETURN THE CONTENTS OF THE CALL
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // RETURN THE CONTENTS OF THE CALL
 	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
- 	curl_setopt($ch, CURLOPT_POST      , true);
- 	curl_setopt($ch, CURLOPT_POSTFIELDS    , $fields);
- 	curl_setopt($ch, CURLOPT_HEADER      ,false);  // DO NOT RETURN HTTP HEADERS 
+ 	curl_setopt($ch, CURLOPT_POST, true);
+ 	curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+ 	curl_setopt($ch, CURLOPT_HEADER, false);  // DO NOT RETURN HTTP HEADERS 
  	$response = curl_exec($ch);
  	
- 	//View the response from the API server
+ 	// View the response from the API server
  	echo $response;
  	
- 	//If no response is sent back from the server, silmply resend this query.
+ 	// If no response is sent back from the server,
+ 	// simply rerun this script until you get a response
 ?>
